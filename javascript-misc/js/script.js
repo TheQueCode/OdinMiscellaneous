e120b5d7817192f077acd78530f4f7d4629bb4ad
TheQueCode <thequecode@gmail.com>
2025-04-27 05:53:13 -0500

    Miscellaneous JS

---
+++ javascript-misc/js/script.js
@@ -0,0 +1,87 @@
+
+console.log("Hello, World!");
+
+/* console.log() is the command to print something to the developer console in your browser */
+
+/*
+let name = "John";
+let surname = "Wick";
+
+console.log(name, surname);
+
+let age = 11;
+console.log(age);
+
+age = 54;
+console.log(age);
+
+const pi = 3.14;
+
+console.log(pi);
+*/
+/* 
+    let can be re-assigned. 
+    const can't be re-assigned
+*/
+
+/*
+console.log(3 + 2) - 76 * (1 + 1);
+console.log(23 + 97);
+console.log(13 + 1110 + 1111 + 50 + 46 + 69);
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+console.log(a);
+
+a = 13;
+console.log(a);
+
+let b = 7 * a;
+console.log(b);
+
+const max = 57;
+const actual = max - 13;
+const percentage = actual/max;
+
+console.log(percentage);
+*/
+/*
+let a = 1, b = 1;
+
+let c = ++a;
+console.log(c);
+
+let d = b++;
+console.log(d);
+*/
+/*
+let a = 2;
+
+let x = 1 + (a *= 2);
+
+console.log(a, x);
+*/
+/*
+console.log("" + 1 + 0); // 10
+console.log("" - 1 + 0); // -1
+console.log(true + false); // 1
+console.log(6 / "3"); // 2
+console.log("2" * "3"); // 6
+console.log(4 + 5 + "px"); // 9px
+console.log("$" + 4 + 5); // $45
+console.log("4" - 2); // 2
+console.log("4px" - 2); // NaN
+console.log("  -9  " + 5); // -9 5
+console.log("  -9  " - 5); // -14
+console.log(null + 1); // 1
+console.log(undefined + 1); // NaN
+console.log(" \t \n" - 2); // -2
+*/
+/*
+let a = +prompt("First number?", 1);
+let b = +prompt("Second number?", 2);
+
+// To fix add +
+
+alert(a + b); 
+*/
