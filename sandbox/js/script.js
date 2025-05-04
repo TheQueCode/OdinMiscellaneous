c99c3c79d43609f4890d7aa7671fd20987c9e10c
TheQueCode <thequecode@gmail.com>
2025-05-04 07:26:56 -0500

    sandbox exercises

--- sandbox/js/script.js
+++ sandbox/js/script.js
@@ -17,12 +17,19 @@ function reverseString(string){
 reverseString("blahblahblah");
 reverseString("hello there");
 
-const removeFromArray = function ( arr, toRemove )
+const removeFromArray = function (arr, ...toRemove)
 {
-  
-}
+  const newArr = [];
+  arr.forEach((item) =>
+  {
+    if (!toRemove.includes(item)) {
+      newArr.push(item);
+    }
+  });
+  return newArr;
+};
 
 
-removeFromArray( console.log([13, 45, 67, 89 ], 13));
-removeFromArray(console.log([1, 2, 3, 4, 5], 1, 2, 3, 4));
-removeFromArray(console.log([ 1, 2, 3, 4 ], 3, 2));
+console.log(removeFromArray([13, 45, 67, 89 ], 13));
+console.log(removeFromArray([1, 2, 3, 4, 5], 1, 2, 3, 4));
+console.log(removeFromArray([ 1, 2, 3, 4 ], 3, 2));
