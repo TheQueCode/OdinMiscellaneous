f3d820f3738f21fa475838250bc898cda2f83af3
TheQueCode <thequecode@gmail.com>
2025-05-05 06:34:37 -0500

    ChessBoard exercise

--- eloquentjs/js/script.js
+++ eloquentjs/js/script.js
@@ -95,7 +95,7 @@ for (i = '#'; i <= '#######'; i+='#'){
 }
 
 let triangle = '';
-for (i = '1'; i <= 7; i++){
+for (i = 1; i <= 7; i++){
   triangle += '#'
   console.log(triangle);
 }
@@ -106,10 +106,30 @@ for (i = 1; i <= 7; i++){
 */
 
 // FizzBuzz
-
+/*
 for (i = 1; i <= 100; i++){
   let output = "";
   if (i % 3 === 0) output += 'Fizz';
   if (i % 5 === 0) output += 'Buzz'
   console.log(output || i);
 }
+*/
+
+// ChessBoard
+/*
+let size = 8;
+let board = "";
+
+for (let i = 0; i < size; i++){
+  for (let j = 0; j < size; j++){
+    if ((i + j) % 2 === 0) {
+      board += '#';
+    } else {
+      board += ' ';
+    }
+  }
+  board += '\n';
+}
+
+console.log(board);
+*/
