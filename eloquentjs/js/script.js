25a678fdb8385134c48fcc10f9730e2186ef6255
TheQueCode <thequecode@gmail.com>
2025-05-05 23:02:48 -0500

    JS eloquent

--- eloquentjs/js/script.js
+++ eloquentjs/js/script.js
@@ -133,3 +133,65 @@ for (let i = 0; i < size; i++){
 
 console.log(board);
 */
+/*
+const square = function (x) {return x * x;};
+console.log(square(12));
+
+const squareArrow = x => x * x;
+console.log(squareArrow(11));
+*/
+
+// Growing Functions
+/*
+function printFarmInventory (cows, chickens)
+{
+  let cowString = String(cows);
+  while (cowString.length < 3) {
+    cowString = '0' + cowString;
+  }
+  console.log(`${cowString} Cows`);
+  let chickenString = String(chickens);
+  while (chickenString.length < 3) {
+    chickenString = '0' + chickenString;
+  }console.log(`${chickenString} Chickens`);
+}
+*/
+/*
+printFarmInventory(7, 11);
+
+function printZeroPaddedWithLabel (number, label)
+{
+  let numberString = String(number);
+  while (numberString.length < 3) {
+    numberString = "0" + numberString;
+  }
+  console.log(`${numberString} ${label}`);
+}
+
+function printFarmInventory (cows, chickens, pigs)
+{
+  printZeroPaddedWithLabel(cows, "Cows");
+  printZeroPaddedWithLabel(chickens, "Chickens");
+  printZeroPaddedWithLabel(pigs, "Pigs");
+}
+
+printFarmInventory(7, 11, 3);
+*/
+
+function zeroPad (number, width)
+{
+  let string = String(number);
+  while (string.length < width) {
+    string = "0" + string;
+  }
+  return string;
+}
+
+function printFarmInventory (cows, chickens, pigs)
+{
+  console.log(`${zeroPad(cows, 3)} Cows`);
+  console.log(`${zeroPad(chickens, 3)} Chickens`);
+  console.log(`${zeroPad(pigs, 3)} Pigs`);
+}
+
+printFarmInventory(7, 16, 3);
