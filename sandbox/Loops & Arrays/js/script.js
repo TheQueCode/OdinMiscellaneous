84ddb51fd90cbfba08c127a116e0883554c8b915
TheQueCode <thequecode@gmail.com>
2025-06-14 14:23:20 -0500

    Active learning

--- sandbox/Loops & Arrays/js/script.js
+++ sandbox/Loops & Arrays/js/script.js
@@ -67,3 +67,68 @@ for (let i = 0; i < catsName.length; i++){
 }
 
 console.log(myFavoriteCats);
+
+// Exiting loops with break
+/*
+const contacts = [
+  "Chris:2232322",
+  "Sarah:3453456",
+  "Bill:7654322",
+  "Mary:9998769",
+  "Dianne:9384975",
+];
+const para = document.querySelector("p");
+const input = document.querySelector("input");
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", () => {
+  const searchName = input.value.toLowerCase();
+  input.value = "";
+  input.focus();
+  para.textContent = "";
+  for (const contact of contacts) {
+    const splitContact = contact.split(":");
+    if (splitContact[0].toLowerCase() === searchName) {
+      para.textContent = `${splitContact[0]}'s number is ${splitContact[1]}.`;
+      break;
+    }
+  }
+  if (para.textContent === "") {
+    para.textContent = "Contact not found.";
+  }
+});
+*/
+// skipping iterations with continue
+
+const para = document.querySelector("p");
+const input = document.querySelector("input");
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", () => {
+  para.textContent = "Output: ";
+  const num = input.value;
+  input.value = "";
+  input.focus();
+  for (let i = 1; i <= num; i++) {
+    let sqRoot = Math.sqrt(i);
+    if (Math.floor(sqRoot) !== sqRoot) {
+      continue;
+    }
+    para.textContent += `${i} `;
+  }
+});
+
+
+// Acive learning
+
+//coun down from 10
+
+for (i = 10; i >= 0; i--){
+  if (i === 10) {
+    console.log(`Countdown`);
+  }
+  console.log(`${i}`);
+  if (i === 0) {
+    console.log(`Blast off!`);
+  }
+}
