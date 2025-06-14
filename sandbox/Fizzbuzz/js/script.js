98e119b2bb07ef417e71b4c64ee631c54c55c7cd
TheQueCode <thequecode@gmail.com>
2025-06-13 22:26:09 -0500

    Add User Input

--- sandbox/Fizzbuzz/js/script.js
+++ sandbox/Fizzbuzz/js/script.js
@@ -1,5 +1,7 @@
-function fizzbuzz () {
-  for (let i = 1; i <= 100; i++) {
+let number = prompt('Enter the number you would like to FizzBuzz to: ')
+
+function fizzbuzz (number) {
+  for (let i = 1; i <= number; i++) {
     let output = '';
     if (i % 3 === 0) output += 'Fizz';
     if (i % 5 === 0) output += 'Buzz';
@@ -7,4 +9,4 @@ function fizzbuzz () {
   }
 }
 
-fizzbuzz();
+fizzbuzz(number);
