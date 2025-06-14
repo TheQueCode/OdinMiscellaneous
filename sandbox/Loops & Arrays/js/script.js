fc6058d1a6a29f2478386aa0b7fc33a8aa055bb3
TheQueCode <thequecode@gmail.com>
2025-06-14 14:15:32 -0500

    loop through

--- sandbox/Loops & Arrays/js/script.js
+++ sandbox/Loops & Arrays/js/script.js
@@ -1,9 +1,23 @@
 const container = document.querySelector('.container');
+const results = document.querySelector('.results');
 
-const btn = document.createElement('button');
-btn.textContent = 'Create';
+const calculateBtn = document.createElement('button');
+calculateBtn.textContent = 'Calculate';
+calculateBtn.addEventListener('click', calculate);
+const clearBtn = document.createElement('button');
+clearBtn.textContent = 'Clear';
+clearBtn.addEventListener('click', () => (results.textContent = ''));
 
-container.appendChild(btn);
+container.insertBefore(calculateBtn, results);
+container.insertBefore(clearBtn, results);
+
+function calculate () {
+  for (let i = 1; i <= 10; i++){
+    const newResult = `${i} x ${i} = ${i * i}`;
+    results.textContent += `${newResult}\n`;
+  }
+  results.textContent += '\nFinished!\n\n';
+}
 
 const cats = ['Leopard', 'Jaguar', 'Tiger', 'Lion', 'Mountain Lion', 'Cheeta'];
 
@@ -23,6 +37,7 @@ console.log(upperCats); // ['LEOPARD', 'JAGUAR', 'TIGER', 'LION', 'MOUNTAIN LION
 
 // use filter() to test each item in a collection & create a new collection containing only items that match (returns a boolean)
 
+/*
 function lCat (cat) {
   return cat.startsWith('L');
 }
@@ -30,3 +45,25 @@ function lCat (cat) {
 const filtered = cats.filter(lCat);
 
 console.log(filtered);
+*/
+
+// More compact
+
+const filtered = cats.filter(cat => cat.startsWith('L'));
+
+console.log(filtered);
+
+
+const catsName = ['Pete', 'Biggles', 'Bojangles', 'Jasmine'];
+
+let myFavoriteCats = 'My cats are called ';
+
+for (let i = 0; i < catsName.length; i++){
+  if (i === catsName.length - 1) {
+    myFavoriteCats += `and ${catsName[i]}.`
+  } else {
+    myFavoriteCats += `${catsName[i]}, `
+  }
+}
+
+console.log(myFavoriteCats);
