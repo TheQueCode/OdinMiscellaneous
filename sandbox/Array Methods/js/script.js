328514c9fda95830cccc95ed8bbc08587db691af
TheQueCode <thequecode@gmail.com>
2025-06-18 15:34:34 -0500

    high order array methods - map

--- sandbox/Array Methods/js/script.js
+++ sandbox/Array Methods/js/script.js
@@ -348,7 +348,78 @@ console.log(oldCompanies);
 
 const decadeCompanies = companies.filter(company => company.end - company.start >= 10);
 console.log(decadeCompanies);
-*/
+
 // high order array method
 
 // map
+
+const arrayMap = [1, 4, 9, 16];
+
+const doubledNumbers = arrayMap.map(number => `Number ${number} Doubled: ${number * 2}`);
+
+console.log(doubledNumbers);
+
+const companies = [
+  {name: 'Company One', category: 'Finance', start: 1981, end: 2004},
+  {name: 'Company Two', category: 'Retail', start: 1992, end: 2008},
+  {name: 'Company Three', category: 'Auto', start: 1999, end: 2007},
+  {name: 'Company Four', category: 'Retail', start: 1989, end: 2010},
+  {name: 'Company Five', category: 'Technology', start: 2009, end: 2014},
+  {name: 'Company Six', category: 'Finance', start: 1987, end: 2010},
+  {name: 'Company Seven', category: 'Auto', start: 1986, end: 1996},
+  {name: 'Company Eight', category: 'Technology', start: 2011, end: 2016},
+  {name: 'Company Nine', category: 'Retail', start: 1981, end: 1989},
+];
+
+// Create an array of company names
+
+const companyNames = companies.map(company => company.name);
+console.log(companyNames);
+
+// Create an array with company and category
+
+const companyCategory = companies.map(company => `Company: ${company.name} - Category: ${company.category}`);
+console.log(companyCategory);
+
+// Create an obj with company and category
+
+const companyInfo = companies.map(company => {
+  return {
+    name: company.name,
+    category: company.category
+  }
+});
+console.log(companyInfo);
+
+// Create an array of the length of each company in years
+
+const companyLength = companies.map(company => `${company.name}: ${company.end - company.start} years`);
+console.log(companyLength);
+
+// Create an array of objects with the name of the length of each company in years
+
+const companyYears = companies.map(company => {
+  return {
+    company: company.name,
+    years: company.end - company.start
+  };
+});
+console.log(companyYears);
+*/
+// chain map methods
+ 
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+const squareAndDoubled = numbers
+  .map(number => Math.sqrt(number))
+  .map(sqrtNum => sqrtNum * 2) //sqrtNum is the numbers from the first map, you can name them anything
+
+console.log(squareAndDoubled);
+
+// chain other methods
+
+const evenDoubled = numbers
+  .filter(number => number % 2 === 0)
+  .map(evenNums => evenNums * evenNums)
+
+console.log(evenDoubled);
