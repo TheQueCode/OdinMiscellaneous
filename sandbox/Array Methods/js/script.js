92f9caef835f325d43ca3a41b73d86e55894b96f
TheQueCode <thequecode@gmail.com>
2025-06-18 17:45:17 -0500

    Array Method Challenges

--- sandbox/Array Methods/js/script.js
+++ sandbox/Array Methods/js/script.js
@@ -444,3 +444,68 @@ const cart = [
 const totalCart = cart.reduce((prev, curr) => prev + curr.price, 0);
 console.log(totalCart);
 */
+
+const people = [
+  {
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'john@gmail.com',
+    phone: '111-111-1111',
+    age: 30,
+  },
+  {
+    firstName: 'Jane',
+    lastName: 'Poe',
+    email: 'jane@gmail.com',
+    phone: '222-222-2222',
+    age: 25,
+  },
+  {
+    firstName: 'Bob',
+    lastName: 'Foe',
+    email: 'bob@gmail.com',
+    phone: '333-333-3333',
+    age: 45,
+  },
+  {
+    firstName: 'Sara',
+    lastName: 'Soe',
+    email: 'sara@gmail.com',
+    phone: '444-444-4444',
+    age: 19,
+  },
+  {
+    firstName: 'Jose',
+    lastName: 'Joe',
+    email: 'jose@gmail.com',
+    phone: '555-555-5555',
+    age: 23,
+  },
+];
+
+const youngPeople = people
+  .filter(person => person.age <= 25)
+  .map(ppl => {
+    return {
+      name: `${ppl.firstName} ${ppl.lastName}`,
+      email: `${ppl.email}`
+    }
+  }
+  );
+  
+console.log(youngPeople);
+
+const numbers = [2, -30, 50, 20, -12, -9, 7];
+
+const positiveNumbers = numbers
+  .filter(numbers => numbers > 0)
+  .reduce((prev, curr) => prev + curr, 0);
+
+console.log(positiveNumbers);
+
+const words = ['coder', 'programmer', 'developer'];
+
+const capitalizedWords = words
+  .map(words => words[0].toUpperCase() + words.slice(1))
+
+console.log(capitalizedWords);
